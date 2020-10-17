b8f3a4f1e1c6520c493945dfe5f19585c39ee749
FlysJoint <flysjoint@gmail.com>
2020-10-17 17:47:32 +0100

    basic day/night cycle more rooms added

--- BtV.py
+++ BtV.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 # TODO:
-# navigation (including not going off the map or through walls)
 # secret tunnels (going through walls!)
 # random attacks
-# day/night cycle
+# day/night cycle - needs to be every 6 moves instead of every other
 # random spawns
 # combat
 # inventory
@@ -16,6 +15,7 @@ moves = 0
 
 class Room(object):
     tunnels = []
+    night = ''
 
     def __init__(self, name, description, exits, pos):
         self.name = name
@@ -24,10 +24,14 @@ class Room(object):
         self.pos = pos
 
     def describe(self):
+        global moves
         print("\033[H\033[J") 
         debug()
-        # print(self.pos)
-        print('You are in the %s. %s' % (self.name, self.description))
+        if moves % 2 == 0:
+            self.night = ''
+        else:
+            self.night = ' at night'
+        print('You are in the %s%s. %s' % (self.name, self.night, self.description))
         prompt()
 
 
@@ -58,6 +62,7 @@ def action(my_action):
         print('That is not a valid direction')
         prompt()
 
+
 def debug():
     global moves
     global current_room
@@ -70,17 +75,43 @@ def find_room(x, y):
         current_room = stacks
     elif x == 1 and y == 0:
         current_room = library
-    elif x == 1 and y == 1:
-        current_room = north_corridor
+    elif x == 2 and y == 0:
+        current_room = cafeteria
+    elif x == 3 and y == 0:
+        current_room = bleachers
+    elif x == 4 and y == 0:
+        current_room = bronze
     elif x == 0 and y == 1:
         current_room = northwest_corridor
+    elif x == 1 and y == 1:
+        current_room = north_corridor
+    elif x == 2 and y == 1:
+        current_room = northeast_corridor
+    elif x == 3 and y == 1:
+        current_room = sports_field
     elif x == 0 and y == 2:
         current_room = basement
+    elif x == 1 and y == 2:
+        current_room = lockers
+    elif x == 2 and y == 2:
+        current_room = restroom
+    elif x == 0 and y == 3:
+        current_room = principal_office
+    elif x == 1 and y == 3:
+        current_room = south_corridor
+    elif x == 2 and y == 3:
+        current_room = computer_room
     else:
         print('Trying to find x:%s, y:%s' % (x, y))
     print('moving to: ' + current_room.name)
     current_room.describe()
 
+stacks = Room(
+    'Stacks',
+    'Piles of musty tomes tower above you.',
+    ['east'],
+    [0, 0]
+    )
 
 library = Room(
     'Library',
@@ -89,12 +120,12 @@ library = Room(
     [1, 0]
     )
 
-stacks = Room(
-    'Stacks',
-    'Piles of musty tomes tower above you.',
-    ['east'],
-    [0, 0]
-    )
+cafeteria = Room(
+    'Cafeteria',
+    'The stench of stale mashed potato hangs heavy in the air.',
+    ['south'],
+    [2, 0]
+)
 
 north_corridor = Room(
     'North Corridor',
@@ -110,11 +141,74 @@ northwest_corridor = Room(
     [0, 1]
 )
 
-basement= Room(
+northeast_corridor = Room(
+    'Northeast Corridor',
+    'Students hustle and bustle on their way to class',
+    ['east', 'south', 'north', 'west'],
+    [2, 1]
+)
+
+basement = Room(
     'Basement',
     'Drips echo off rusty metal pipes. You can hear a rat scurrying. Amy?',
     ['north'],
     [0, 2]
+)
+
+restroom = Room(
+    'Restroom',
+    'A long mirror reflects the broken cubicles opposite. Some students giggle as you enter and run out.',
+    ['north'],
+    [2, 2]
+)
+
+sports_field = Room(
+    'Sports Field',
+    'Jocks run plays while cheerleaders dance their acrobatic routines',
+    ['north', 'west', 'east'],
+    [3, 1]
+)
+
+bleachers = Room(
+    'Bleachers',
+    'Sicknotes watch on as the athletes run laps',
+    ['south'],
+    [3, 0]
+)
+
+bronze = Room(
+    'Bronze',
+    'Indie bands rock out to the tune of pool balls being sunk and drinks being drunk',
+    ['south'],
+    [4, 0]
+)
+
+lockers = Room(
+    'Lockers',
+    'Locker doors slam open and shut, a constant cacophony of clamour',
+    ['south', 'north'],
+    [1, 2]
+)
+
+south_corridor = Room(
+    'South Corridor',
+    'Students hustle and bustle on their way to class',
+    ['east', 'south', 'north', 'west'],
+    [1, 3]
+)
+
+principal_office = Room(
+    'Principal\'s Office',
+    'The throne room of the most evil demon of them all.',
+    ['east'],
+    [0, 3]
+)
+
+computer_room = Room(
+    'Computer Room',
+    'Beeps and boops can be heard over the noise of blistering 14.4 modems',
+    ['west'],
+    [2, 3]
 )
 
 find_room(1, 0)
