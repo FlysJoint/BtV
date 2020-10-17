ad34fead50f70c226d5c136acc5ed5ab68894e97
FlysJoint <flysjoint@gmail.com>
2020-10-17 16:51:21 +0100

    quit msg added third room added north/south nav added clear screen on room entry added

--- BtV.py
+++ BtV.py
@@ -12,7 +12,6 @@
 map = []
 global current_room
 
-
 class Room(object):
     tunnels = []
 
@@ -23,38 +22,46 @@ class Room(object):
         self.pos = pos
 
     def describe(self):
+        print("\033[H\033[J") 
         print(self.pos)
         print(self.description)
-        print('There are exits: %s' % self.exits)
+        # print('There are exits: %s' % self.exits)
         prompt()
-        
+
 
 def prompt():
+    print('There are exits: %s' % current_room.exits)
     my_action = input('What do you want to do? : ')
     action(my_action)
 
+
 def action(my_action):
     global current_room
-    print(current_room.name)
-    print(current_room.pos)
-    print(current_room.exits)
     if my_action in current_room.exits:
-        print('going %s...' % my_action)
+        print('going %s from %s...' % (my_action, current_room.name))
         if my_action == 'west':
             find_room(current_room.pos[0] - 1, current_room.pos[1])
-        if my_action == 'east':
+        elif my_action == 'east':
             find_room(current_room.pos[0] + 1, current_room.pos[1])
+        elif my_action == 'north':
+            find_room(current_room.pos[0], current_room.pos[1] - 1)
+        elif my_action == 'south':
+            find_room(current_room.pos[0], current_room.pos[1] + 1)
+    elif my_action == 'quit':
+        print('You run away, leaving the innocent people of Sunnydale to fend for themselves. They all die.')
     else:
         print('That is not a valid direction')
+        prompt()
+
 
 def find_room(x, y):
     global current_room
     if x == 0 and y == 0:
         current_room = stacks
-        # print('moving to stacks')
     elif x == 1 and y == 0:
         current_room = library
-        # print('moving to library')
+    elif x == 1 and y == 1:
+        current_room = north_corridor
     else:
         print('Trying to find x:%s, y:%s' % (x, y))
     print('moving to: ' + current_room.name)
@@ -74,5 +81,12 @@ stacks = Room(
     ['east'],
     [0, 0]
     )
+
+north_corridor = Room(
+    'North Corridor',
+    'You are in the North Corridor. Students hustle and bustle on their way to class',
+    ['north', 'west', 'east', 'south'],
+    [1, 1]
+)
 
 find_room(1, 0)
